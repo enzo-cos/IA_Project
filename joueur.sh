bf698d5d446515bc8d6fccb166f8dbe9253243c7
Gurbulak Bilal <oxizone25@gmail.com>
2022-05-16 05:57:48 +0200

    joueur.sh

--- joueur.sh
+++ joueur.sh
@@ -1,7 +1,14 @@
-host=$1
-port=$2
-nom=$3
-portIA=$4
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <host> <port> <nom_joueur> <portIA>"
+    exit 1
+else
+    host=$1
+    port=$2
+    nom=$3
+    portIA=$4
+fi
 
 
 ./truePlayer $host $port $nom $portIA &
